d01e823384a73df215410086201a1bcba5a0813e
Romarick Picardat--Charrière <romarick.picardat--charrire@etu.uca.fr>
2026-06-11 10:49:10 +0200

    Prepare preload-scene for new scene

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { PreloadScene } from './scenes/preload-scene.js';
 import { IntroScene } from './scenes/intro-scene.js';
 import { LevelChoiceScene } from './scenes/level-choice-scene.js';
 import { MapScene } from './scenes/map-scene.js';
+import { CorrectionScene } from './scenes/correction-scene.js';
+import { SolutionScene } from './scenes/solution-scene.js';
 
 const CONFIG = {
   type: Phaser.AUTO,
@@ -20,7 +22,7 @@ const CONFIG = {
       debug: false,
     },
   },
-  scene: [PreloadScene, IntroScene, LevelChoiceScene, MapScene],
+  scene: [PreloadScene, IntroScene, LevelChoiceScene, MapScene, CorrectionScene, SolutionScene],
 };
 
 new Phaser.Game(CONFIG);
